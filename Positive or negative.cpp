cd5fd1e4bc573789c169507d16359d5015a539db
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-17 14:53:18 +0530

    Add files via upload

---
+++ Positive or negative.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter an integer value : " <<endl;
+    cin>>a;
+    
+    if(a>0)
+    {
+        cout<<"A is positive" <<endl;
+    }
+    else
+    {
+        cout<<"A is negative" <<endl;
+    }
+
+    return 0;
+}

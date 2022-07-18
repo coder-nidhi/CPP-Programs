953bae83636ad9af593b83f62326190263539eaf
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-18 20:53:37 +0530

    Add files via upload

---
+++ Odd number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter a number" <<endl;
+    cin>>a;
+    
+    if(a%2==1)
+    {
+        cout<<"It is an odd number";
+    }
+    return 0;
+}

392ca7e0cce404d52c82c7221cab0c499fb6f060
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-23 19:10:20 +0530

    Add files via upload

---
+++ Subtraction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter two numbers for subtraction : " <<endl;
+    int a, b;
+    cin>>a >>b;
+    
+    int result = a-b;
+    cout << "Subtraction : " << result << endl;
+    return 0;
+}

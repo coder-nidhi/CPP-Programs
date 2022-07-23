3ef8b632240bde33f1b43ed36b715cfe48a0a99c
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-23 19:09:53 +0530

    Add files via upload

---
+++ Product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter two numbers to find their product : " <<endl;
+    int a, b;
+    cin>>a >>b;
+    
+    int result = a*b;
+    cout << "Product : " << result << endl;
+    return 0;
+}

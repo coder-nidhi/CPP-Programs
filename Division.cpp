c900550cb7e28dd10de3ca8d928354bae5080502
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-24 22:23:38 +0530

    Add files via upload

---
+++ Division.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter two numbers for division : " <<endl;
+    int a, b;
+    cin>>a >>b;
+    
+    int result = a/b;
+    cout << "Division : " << result << endl;
+    return 0;
+}
+

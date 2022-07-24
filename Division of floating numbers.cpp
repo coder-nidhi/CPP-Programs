54299205ad7885abe050c9caa7a4db366df5c817
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-24 22:22:43 +0530

    Add files via upload

---
+++ Division of floating numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter two numbers to find their division : " <<endl;
+    float a, b;
+    cin>>a >>b;
+    
+    float result = a/b;
+    cout << "Division : " << result << endl;
+    return 0;
+}
+

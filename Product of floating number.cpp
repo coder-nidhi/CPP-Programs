1ed3465d479a46d35fd5f347d2b6b7ea52e522cf
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-24 22:23:57 +0530

    Add files via upload

---
+++ Product of floating number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter two numbers to find their product : " <<endl;
+    float a, b;
+    cin>>a >>b;
+    
+    float result = a*b;
+    cout << "Product : " << result << endl;
+    return 0;
+}
+

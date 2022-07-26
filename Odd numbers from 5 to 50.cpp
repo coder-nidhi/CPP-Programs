bef7517f1fdc325496e4c2aca487c9be3bf8a532
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-26 20:21:40 +0530

    Add files via upload

---
+++ Odd numbers from 5 to 50.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i = 5;i<=50;i++)
+    {
+        if(i%2!=0){
+            cout<<i <<endl;
+        }
+    }
+    return 0;
+}

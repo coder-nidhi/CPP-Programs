9075c61d46d52f54e8246e81b3762ae070855221
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-02 20:54:32 +0530

    Add files via upload

---
+++ Simple Interest.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float principle,time, rate, SI;
+   
+    cout<<"Enter principle : \n";
+    cin>>principle;
+    
+    cout<<"Enter principle : \n";
+    cin>>time; 
+    
+    cout<<"Enter principle : \n";
+    cin>>rate;
+    
+    SI=principle*time*rate/100;
+    
+    cout<<"Simple Interest is "<<SI;
+    
+    return 0;
+}

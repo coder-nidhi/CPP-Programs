e2492c530601cca3116e97021be97ecd3777607b
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-02 20:54:47 +0530

    Add files via upload

---
+++ Square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, square;
+    
+    cout<<"Enter a number to find its square : ";
+    cin>>n;
+    
+    square = n*n;
+    
+    cout<<"Square : "<<square<<endl;
+    
+    
+
+    return 0;
+}

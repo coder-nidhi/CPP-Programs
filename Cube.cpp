965446ecbbe9de86c711990dfcebfbdc64031a7a
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-02 20:54:17 +0530

    Add files via upload

---
+++ Cube.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, cube;
+    
+    cout<<"Enter a number to find it's cube : ";
+    cin>>n;
+    
+    cube = n*n*n;
+    
+    cout<<"Cube of "<< n <<" is "<<cube<<endl;
+    
+    return 0;
+}

575fd128cc8e55f833627bca5b62270231506366
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-03 22:13:32 +0530

    Add files via upload

---
+++ Alphabets(A to Z).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char i;
+    
+    cout<<"The alphabets from A to Z are : "<<endl;
+    
+    for(i='A';i<='Z';i++){
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}

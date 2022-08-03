5e8bee2ab66c16c0bf39b324f62c7963e7666faa
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-03 22:13:48 +0530

    Add files via upload

---
+++ Average of 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, c, avg;
+    
+    cout<<"Enter three numbers to find it's average : "<<endl;
+    cin>>a>>b>>c;
+    
+    avg=(a+b+c)/3;
+    cout<<"Average 0f the 3 numbers is : "<<avg;
+
+    return 0;
+}

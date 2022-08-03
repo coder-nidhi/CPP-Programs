f9fb786d9929aa1835f012715308d5c8af273ac5
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-03 22:14:35 +0530

    Add files via upload

---
+++ Perimeter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float l,w,perimeter;
+    
+    cout<<"Enter length and width to find perimeter : " <<endl;
+    cin>>l>>w;
+    
+    perimeter=(2*l)+(2*w);
+    cout<<"Perimeter = "<<perimeter;
+    
+
+    return 0;
+}

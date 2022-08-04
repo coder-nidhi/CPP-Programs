060d92e4abef8b86577d62732c9942200f4c8b23
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-04 18:36:36 +0530

    Add files via upload

---
+++ Area.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int L, B, area;
+    
+    cout<<"Enter length : ";
+    cin>>L;
+    
+    cout<<"Enter breadth : ";
+    cin>>B;
+    
+    area=L*B;
+    
+    cout<<"The area is : "<<area;
+    
+    return 0;
+}

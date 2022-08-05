726b3e9780c772fb99c6426e289b282b05727ee5
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-05 20:05:05 +0530

    Add files via upload

---
+++ Multiplication Table.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number, i, ans;
+    
+    cout<<"Enter any number : ";
+    cin>>number;
+    
+    for(int i = 1; i<=10; i++){
+        cout<<number*i<<endl;
+    }
+
+    return 0;
+}

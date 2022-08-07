34549c83a6f2a41a14d304007f6be33d8847e230
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-07 22:42:31 +0530

    Add files via upload

---
+++ Natural number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, n;
+    
+    cout<<"Enter a number : ";
+    cin>>n;
+    
+    if(i>=0){
+        cout<<"Natural number!";
+    }
+    else{
+        cout<<"Not a natural number!";
+    }
+    return 0;
+}

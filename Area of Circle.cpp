2c01dca01ae0ad47dd8f4c63968707e060c495fd
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-09 21:50:33 +0530

    Add files via upload

---
+++ Area of Circle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float r, Area; 
+    
+    cout<<"Enter the radius of circle to find it's area :  ";
+    cin>>r;
+    
+    Area = 3.14*r*r;
+    
+    cout<<"The area of circle is : "<<Area;
+    
+    return 0;
+}

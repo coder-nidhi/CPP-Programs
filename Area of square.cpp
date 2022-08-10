a20aedfbe4502256d71ba79df16e43051c0fb8ec
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-10 22:58:48 +0530

    Add files via upload

---
+++ Area of square.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int area, side;
+    
+    cout<<"Enter the side of square to find it's area : ";
+    cin>>side;
+    
+    area = side * side;
+    
+    cout<<"Area of Square = "<<area;
+    
+    return 0;
+}

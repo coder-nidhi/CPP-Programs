d85f514c1364d00f5f6565c8ca33d731a87c01fc
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-10 22:58:32 +0530

    Add files via upload

---
+++ Area of parellelogram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int base, height, area;
+    
+    cout<<"Enter the base of parellelogram  : ";
+    cin>>base;
+    
+    cout<<"Enter the height of parellelogram  : ";
+    cin>>height;
+    
+    area = base*height;
+    
+    cout<<"Area of parellelogram = "<<area;
+    
+    return 0;
+}

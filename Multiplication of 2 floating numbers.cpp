f79e5ef4c3ecfe1413b0a53b7f9ffd2151387e2e
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-11 21:09:28 +0530

    Add files via upload

---
+++ Multiplication of 2 floating numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b, multiplication;
+    
+    cout<<"Enter the value of a and b : ";
+    cin>>a>>b;
+    
+    multiplication = a*b;
+    
+    cout<<"Multiplication = "<<multiplication;
+
+    return 0;
+}

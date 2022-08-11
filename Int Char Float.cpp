1ec94bb0b6b6d0733d5f0ca188b6a2422ea489a3
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-11 21:08:12 +0530

    Add files via upload

---
+++ Int Char Float.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  
+    int Integer;
+    char Character;
+    float InputFloat;
+ 
+    cout<<"Please enter a character : \n";
+    cin>>Character;
+  
+    cout<<"Please enter an integer value : \n";
+    cin>>Integer;
+  
+    cout<<"Please enter float value :  ";
+    cin>>InputFloat;    
+  
+    cout<<"The Integer Value that you Entered is : "<<Integer<<endl;
+    cout<<"The Character that you Entered is : "<< Character<<endl;
+    cout<<"The Float Value that you Entered is : "<<InputFloat<<endl;
+  
+    return 0;
+}

142cdca767eb8bbad6d36a58bc33bf9b6594cb49
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-12 22:18:10 +0530

    Add files via upload

---
+++ Circumference of circle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float ci, rad;
+    
+    cout<<"Enter radius : ";
+    cin>>rad;
+    
+    ci = 2 * 3.14 * rad;
+    
+    cout<<"Circumference of the circle is : "<<ci;
+    return 0;
+}

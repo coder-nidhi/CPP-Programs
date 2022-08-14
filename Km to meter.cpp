013eb5d3c31a65e505dadd2bd0abe1dd2c64c04a
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-14 20:25:37 +0530

    Add files via upload

---
+++ Km to meter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float km,meter;
+    
+    cout<<"Enter the distance in kilometers"<<endl;
+    cin>>km;
+    
+    meter = (km*1000);
+    
+    cout<<"Meters = "<<meter;
+    
+    return 0;
+}

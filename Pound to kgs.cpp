c4eb5ec8bc0f78d10cb87e2420e6f7c51639b140
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-15 22:46:18 +0530

    Add files via upload

---
+++ Pound to kgs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float pound,kg;
+    
+    cout<<"Enter in pound : ";
+    cin>>pound;
+    
+    kg = (pound*0.453592);
+    
+    cout<<"pound = "<<kg;
+    return 0;
+}

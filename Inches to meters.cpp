e8b410a97b3c02becbcdfd1e52d6f39b8eb0adc4
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-15 22:45:48 +0530

    Add files via upload

---
+++ Inches to meters.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float inches,meters;
+    
+    cout<<"Enter in inches"<<endl;
+    cin>>inches;
+    
+    meters = (inches*0.0254);
+    
+    cout<<"Meters= "<<meters;
+
+
+    return 0;
+}

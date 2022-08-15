3b2b1b5b67cebc802571ebf9495491a8a9379bdf
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-08-15 22:46:05 +0530

    Add files via upload

---
+++ Km to Miles.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float km, miles;
+    
+    cout<<"Enter the distance in kilometers"<<endl;
+    cin>>km;
+    
+    miles = (km*0.621371);
+    cout<<"miles = "<<miles;
+    
+    return 0;
+}
